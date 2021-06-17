3acd9cfaf9a82d752485a7fb3b7eb5528185528c
Spacestronaut <Abelr6099@gmail.com>
2021-06-17 09:00:00 +0700

    first vue commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Counter></Counter>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>{{ number }}</h1>
+        <button @click="increment" class="btn btn-primary">+</button>
+        <button @click="decrement" class="btn btn-primary ms-3">-</button>
+    </div>
+</template>
+
+<script>
+    export default({
+        name : "Counter",
+        data() {
+            return {
+                number: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.number = this.number + 1
+            },
+            decrement() {
+                this.number -= 1;
+
+                if (this.number < 0) {
+                    this.number = 0
+                }
+            }
+        },
+    })
+</script>
+
+<style scoped>
+
+</style>
